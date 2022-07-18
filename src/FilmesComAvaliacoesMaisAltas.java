1cf9328bc445da7c60788a3a889628e8bf953a6b
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-07-18 15:55:22 -0300

    Finalização da classe de filmes com avaliações mais altas

--- src/FilmesComAvaliacoesMaisAltas.java
+++ src/FilmesComAvaliacoesMaisAltas.java
@@ -3,11 +3,10 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
-import java.security.Key;
 import java.util.List;
 import java.util.Map;
 
-public class App {
+public class FilmesComAvaliacoesMaisAltas {
     public static void main(String[] args) throws Exception {
         //fazer uma conexão HTTP e buscar os top 250 filmes
         String url = "https://api.mocki.io/v2/549a5d8b";
@@ -23,8 +22,16 @@ public class App {
         System.out.println("Total de filmes: " + listaDeFilmes.size());
         System.out.println("Primeiro filme: " + listaDeFilmes.get(0));
         //exibir e manipular os dados
+        System.out.println("----------|Filmes com avaliações mais altas|----------");
         for (Map<String, String> filme : listaDeFilmes) {
-            System.out.println(filme.get("title"));
+            System.out.println("Filme: " + filme.get("title"));
+            System.out.println("Ano de lançamento: " + filme.get("year"));
+            System.out.println("Link do poster: " + filme.get("image"));
+            System.out.println("Elenco: " + filme.get("crew"));
+            System.out.println("Nota final: " + filme.get("imDbRating"));
+            System.out.println("Notas dadas: " + filme.get("imDbRatingCount"));
+            System.out.println();
         }
+        System.out.println("--------------------------------------------------------");
     }
 }
