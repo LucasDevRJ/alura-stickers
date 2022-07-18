bbc0b63d8c4128bac5d0b42d5ce7715132b49f4d
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-07-18 13:15:00 -0300

    Adição da API no código

--- src/App.java
+++ src/App.java
@@ -1,5 +1,22 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //fazer uma conexão HTTP e buscar os top 250 filmes
+        String url = "https://api.mocki.io/v2/549a5d8b";
+        URI endereco = URI.create(url);
+        HttpClient cliente = HttpClient.newHttpClient();
+        HttpRequest solicitacao = HttpRequest.newBuilder(endereco).GET().build();
+        HttpResponse<String> resposta = cliente.send(solicitacao, BodyHandlers.ofString());
+        resposta.body();
+        String conteudo = resposta.body();
+        System.out.println(conteudo);
+        //extrair só os dados que interessam (título, poster, classificação)
+
+        //exibir e manipular os dados
     }
 }
